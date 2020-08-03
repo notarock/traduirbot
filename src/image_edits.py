ff936756a6a6409a219b8f98370ec770233d98d0
Roch D'amour <roch.damour@gmail.com>
2020-08-03 01:16:32 -0400

    Make this code a bit less ugly

--- src/image_edits.py
+++ src/image_edits.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+from wand.color import Color
+from wand.drawing import Drawing
+from wand.image import Image
+
+
+def write_on_image(filename, translation_result, output_file):
+    x_1 = translation_result.vertices[0].x
+    y_1 = translation_result.vertices[0].y
+    x_2 = translation_result.vertices[2].x
+    y_2 = translation_result.vertices[2].y
+
+    with Image(filename=filename) as img:
+        with Drawing() as draw:
+            draw.stroke_color = Color('white')
+            draw.fill_color = Color('white')
+            draw.rectangle(left=x_1, top=y_1, right=x_2, bottom=y_2)
+            draw(img)
+
+        with Drawing() as draw:
+            draw.font = 'wandtests/assets/League_Gothic.otf'
+            draw.font_size = 20
+            draw.text(x_1, y_1, translation_result.description)
+            draw.draw(img)
+
+        img.save(filename=output_file)
--- src/main.py
+++ src/main.py
@@ -2,11 +2,10 @@
 
 import sys
 
-from file_detection import detect
-from wand.drawing import Drawing
-from wand.color import Color
 from google.cloud import translate_v2 as translate
-from wand.image import Image
+
+from file_detection import detect
+from image_edits import write_on_image
 
 
 def translate_to(source_text, target_lang):
@@ -30,31 +29,11 @@ if __name__ == '__main__':
     print("Translating " + path)
 
     api_result = detect(path)
-    source_text = translate_to(api_result.description, target_lang)
-    print("source text: " + source_text)
+    translation_result = translate_to(api_result.description, target_lang)
+    print("source text: " + translation_result.description)
 
     vertices = api_result.bounding_poly.vertices
 
-    print(vertices)
-
-    x1 = vertices[0].x
-    y1 = vertices[0].y
-    x2 = vertices[2].x
-    y2 = vertices[2].y
-
-    with Image(filename=path) as img:
-        with Drawing() as draw:
-            draw.stroke_color = Color('white')
-            draw.fill_color = Color('white')
-            draw.rectangle(left=x1, top=y1, right=x2, bottom=y2)
-            draw(img)
-
-        with Drawing() as draw:
-            draw.font = 'wandtests/assets/League_Gothic.otf'
-            draw.font_size = 20
-            draw.text(x1, y1, source_text)
-            draw.draw(img)
-
-        img.save(filename='test.png')
+    write_on_image(path, translation_result, 'out.png')
 
     print("um-k, sounds like im done.")
