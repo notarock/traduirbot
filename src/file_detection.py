a0ca34aec6ffb703a43a1bb02480781b6b956931
Roch D'amour <roch.damour@gmail.com>
2020-08-02 23:32:47 -0400

    2 files because that was ugly

--- src/file_detection.py
+++ src/file_detection.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+
+import io
+
+from google.cloud import vision
+
+
+def detect(path):
+    """
+    Uses Google's vision API to get the text
+    that is written in the image and its position.
+    """
+    vision_client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = vision_client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+    # for text in texts:
+    #     print('\n"{}"'.format(text.description))
+    #     vertices = (['({},{})'.format(vertex.x, vertex.y)
+    #         for vertex in text.bounding_poly.vertices])
+    #     print('bounds: {}'.format(','.join(vertices)))
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+    return texts[0].description
--- src/main.py
+++ src/main.py
@@ -1,42 +1,11 @@
 #!/usr/bin/env python3
 
-import io
 import sys
 
 from google.cloud import translate_v2 as translate
-from google.cloud import vision
+from file_detection import detect
 
 PATH = './memes/en/char-spag.png'
-
-
-def detect(path):
-    """
-    Uses Google's vision API to get the text
-    that is written in the image and its position.
-    """
-    vision_client = vision.ImageAnnotatorClient()
-
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-
-    image = vision.types.Image(content=content)
-
-    response = vision_client.text_detection(image=image)
-    texts = response.text_annotations
-    print('Texts:')
-    # for text in texts:
-    #     print('\n"{}"'.format(text.description))
-    #     vertices = (['({},{})'.format(vertex.x, vertex.y)
-    #         for vertex in text.bounding_poly.vertices])
-    #     print('bounds: {}'.format(','.join(vertices)))
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
-
-    return texts[0].description
-
 
 def translate_to(source_text, target_lang):
     """
