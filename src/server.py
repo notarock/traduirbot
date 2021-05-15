6d5a695d6475aa867d643d1116bf81b83138f183
Roch D'Amour <roch.damour@gmail.com>
2021-05-15 02:28:01 -0400

    Save picture

--- src/server.py
+++ src/server.py
@@ -1,20 +1,37 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+import pprint
+import os
 
 PORT = 8081
 
-def hello_world(request):
+def homepage(request):
     _resp = Response()
     _resp.headerlist =  [('Content-type',"text/html; 'charset=UTF-8'")]
-    html = open('src/views/page.html','rb').read()
-    _resp.body = html
+    _resp.body = open('src/views/page.html','rb').read()
     return _resp
 
+def traduir(request):
+    request.storage.save(request.POST['meme'])
+
+    _resp = Response()
+    _resp.headerlist =  [('Content-type',"image/png; 'charset=UTF-8'")]
+    _resp.body = open(meme,'rb').read()
+    return _resp
+
+settings = {
+    'storage.base_path': "/tmp/traduir",
+    'storage.extensions': "images+archives",
+}
+
 if __name__ == '__main__':
-    with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
+    with Configurator(settings=settings) as config:
+        config.include('pyramid_storage')
+        config.add_route('home', '/')
+        config.add_route('traduir', '/traduir')
+        config.add_view(homepage, route_name='home')
+        config.add_view(traduir, route_name='traduir')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', PORT, app)
     print("Server listening on port " + str(PORT))
